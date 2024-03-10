d5a161d4bcb04f41f43c7f251505d86ee9eaf4fd
cataghiglione <catalina.ghiglione@ing.austral.edu.ar>
2024-03-10 17:49:01 -0300

    finished lexer implementation

--- src/main/kotlin/token/Division.kt
+++
@@ -1,6 +0,0 @@
-package org.example.token
-
-import org.example.position.Position
-
-data class Division(val start: Position, val end: Position) : Token {}
-
--- src/main/kotlin/token/Sum.kt
+++
@@ -1,5 +0,0 @@
-package org.example.token
-
-import org.example.position.Position
-
-data class Sum(val start: Position, val end: Position) : Token {}
