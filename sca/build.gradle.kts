439df8420a5dba24723ad9befbed1c71b0b63e8d
Matías Bregoli <matias.bregoli@ing.austral.edu.ar>
2024-03-24 18:44:43 -0300

    Updated sca's build.gradle to be in groovy

--- sca/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("printscript.gradle.kotlin-common-conventions")
-}
-
-dependencies {
-    implementation(project(":common"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0")
-}
